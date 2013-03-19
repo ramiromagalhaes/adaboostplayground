16323fa00e87ca1938a3327675dbde913e10ae99
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-03-19 11:38:01 -0300

    Mudando o processo de build.

---
+++ Default/Main.d
@@ -0,0 +1,12 @@
+Main.d: ../Main.cpp ../Common.h ../WeakLearner.h ../WeakHypothesis.h \
+ ../StrongHypothesis.h ../Adaboost.h
+
+../Common.h:
+
+../WeakLearner.h:
+
+../WeakHypothesis.h:
+
+../StrongHypothesis.h:
+
+../Adaboost.h:
