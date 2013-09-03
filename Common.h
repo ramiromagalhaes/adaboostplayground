f8e5f96c7e36888e4a83458de31d3e2db31c3a8e
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-09-03 19:24:46 -0300

    New type: weight_type. Comments.

--- Common.h
+++ Common.h
@@ -3,11 +3,26 @@
 
 
 
+/**
+ * @brief weight_type Weighs used for training should adopt this type.
+ *                    We use float as a default.
+ */
+typedef float weight_type;
+
+
+
+/**
+ * @brief The Classification enum holds default values for the binary classification case.
+ */
 enum Classification {
 	no = -1, yes = 1
 };
 
-//"typedef"
+
+
+/**
+ * Holds the sample and its classification.
+ */
 template < typename dataType > class LabeledExample {
 public:
 	dataType example;
