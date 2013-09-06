a6dc2a221b722d7079f43550727649ef9fc56b07
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-09-06 11:48:28 -0300

    StrongHypothesis now may write things to a stream.

--- StrongHypothesis.h
+++ StrongHypothesis.h
@@ -2,6 +2,9 @@
 #define STRONGHYPOTHESIS_H_
 
 #include <vector>
+#include <string>
+#include <fstream>
+
 #include "Common.h"
 
 
@@ -24,22 +27,43 @@ private:
 
         entry() : weight(0), weakHypothesis(0) {}
         entry(weight_type w, WeakHypothesis const * h) : weight(w),
-                                                         weakHypothesis(h) {
-
-        }
-        ~entry() {
+                                                         weakHypothesis(h) {}
+        ~entry()
+        {
             delete weakHypothesis;
         }
     };
 
     std::vector<entry> hypothesis;
 
+    bool writeToStreamOnInsert;
+    std::ofstream data; //this will be kept open during this object's whole life.
+
 public:
     //note: intentional inline methods thanks to templates. See http://stackoverflow.com/questions/644397/c-class-with-template-cannot-find-its-constructor
-    StrongHypothesis() {}
+    StrongHypothesis() {
+        writeToStreamOnInsert = false;
+    }
 
-     ~StrongHypothesis() {
+
+
+    StrongHypothesis(std::string path) {
+        data.open( path.c_str() );
+        if (!data.is_open())
+        {
+            throw 220;
+        }
+        writeToStreamOnInsert = true;
+    }
+
+
+
+    ~StrongHypothesis() {
         hypothesis.clear(); //each entry object destroys its contents
+        if (data.is_open())
+        {
+            data.close();
+        }
     }
 
 
@@ -48,6 +72,14 @@ public:
         //TODO copy the weak_hypothesis
         entry e(alpha, const_cast<WeakHypothesis const *>(weak_hypothesis)); //from now on, this class is responsible for handling properly these pointers
         hypothesis.insert(hypothesis.end(), e);
+
+        if (writeToStreamOnInsert)
+        {
+            data << alpha << " ";
+            weak_hypothesis->write(data);
+            data << std::endl;
+            data.flush();//I think std::endl flushes data
+        }
     }
 
 
@@ -62,6 +94,7 @@ public:
 
         return result >= 0 ? yes : no;
     }
+
 
 
     //TODO accessor methods to the weak hypothesis
