c39d95930c3aef39726026c9c42ef5c14d5e89b4
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-09-17 03:00:24 -0300

    ...

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./adaboosttrain /mnt/faces.txt /mnt/fundo/background-partial.pgm /mnt/srfs/haarclassifiers.txt /mnt/strongHypothesis.txt 10
+./trainclassifier /mnt/faces.txt /mnt/fundo/background-partial.pgm /mnt/srfs/haarclassifiers.txt /mnt/strongHypothesis.txt 10
