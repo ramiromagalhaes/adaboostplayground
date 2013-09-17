8448de6a3d3d80316976c90f7bfdc6ab645a4be1
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-09-17 03:00:24 -0300

    ...

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./adaboosttrain /mnt/faces.txt /mnt/fundo/0.pgm /mnt/srfs/haarclassifiers.txt /mnt/strongHypothesis.txt 200
+./trainclassifier /mnt/faces.txt /mnt/fundo/0.pgm /mnt/srfs/haarclassifiers.txt /mnt/strongHypothesis.txt 200
