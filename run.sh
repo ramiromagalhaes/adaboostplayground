9f98f3a58f64c8390cef15b7e455dff930a185d0
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-09-22 22:42:10 -0300

    new paths

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-./trainclassifier /mnt/faces/faces.pgm /mnt/fundo/background-v2.pgm /mnt/features/haarclassifiers-optimized.txt /mnt/classifiers/strongHypothesis.txt 20
+./train_vj_classifier     /mnt/faces/faces.pgm /mnt/fundo/background-v2.pgm /mnt/features/haarwavelets-original.txt /mnt/classifiers/strongHypothesis-vj.txt 200;
+./train_pavani_classifier /mnt/faces/faces.pgm /mnt/fundo/background-v2.pgm /mnt/features/haarclassifiers-optimized.txt /mnt/classifiers/strongHypothesis-pavani.txt 200;
+./train_my_classifier     /mnt/faces/faces.pgm /mnt/fundo/background-v2.pgm /mnt/features/haarclassifiers-optimized.txt /mnt/classifiers/strongHypothesis-my.txt 200;
