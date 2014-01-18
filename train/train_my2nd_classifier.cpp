7be92f5bb3e886c3465f3ee7ed3c1a147deefe0e
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2014-01-18 16:38:48 -0200

    New training program.

---
+++ train/train_my2nd_classifier.cpp
@@ -0,0 +1,28 @@
+#include "template_trainclassifier.h"
+
+/**
+ * Arguments:
+ *     positivesIndexFile
+ *     positivesImageFile
+ *     negativesIndexFile
+ *     negativesImageFile
+ *     waveletsFile
+ *     strongHypothesisOutputFile
+ *     maximumIterations
+ */
+int main(int, char **argv) {
+    const std::string positivesFile = argv[1];
+    const std::string negativesFile = argv[2];
+    const std::string negativesIndexFile = argv[3];
+    const std::string waveletsFile = argv[4];
+    const std::string strongHypothesisFile = argv[5];
+    const unsigned int maximum_iterations = charToInt(argv[6]);
+
+    ___main<BayesianHaarClassifier, SimpleSelectionWeakLearner<BayesianHaarClassifier> >(
+                positivesFile,
+                negativesFile,
+                negativesIndexFile,
+                waveletsFile,
+                strongHypothesisFile,
+                maximum_iterations);
+}
