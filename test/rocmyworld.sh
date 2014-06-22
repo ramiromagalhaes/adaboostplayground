081658d00e71ded33978969bf4d7f2586cfc82e0
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2014-06-22 18:44:38 -0300

    Removing useless scripts.

--- test/rocmyworld.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-for FILE_NBR in 020 040 060 080 100 120 140 160 180 200; do ./test_my_classifier     /mnt/test-dataset/mit-cmu-abc.index /mnt/test-dataset/mit-cmu-abc.groundtruth /home/ramiro/workspace/classifiers/strongHypothesis-my-version3-$FILE_NBR.txt /home/ramiro/roc-my-version3-$FILE_NBR.txt; done;
-for FILE_NBR in 020 040 060 080 100 120 140 160 180 200; do ./test_pavani_classifier /mnt/test-dataset/mit-cmu-abc.index /mnt/test-dataset/mit-cmu-abc.groundtruth /home/ramiro/workspace/classifiers/strongHypothesis-pavani-$FILE_NBR.txt      /home/ramiro/roc-pavani-$FILE_NBR.txt; done;
-for FILE_NBR in 020 040 060 080 100 120 140 160 180 200; do ./test_vj_classifier     /mnt/test-dataset/mit-cmu-abc.index /mnt/test-dataset/mit-cmu-abc.groundtruth /home/ramiro/workspace/classifiers/strongHypothesis-vj-$FILE_NBR.txt          /home/ramiro/roc-vj-$FILE_NBR.txt; done;
--- test/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./test_my_classifier     /mnt/test-dataset/mit-cmu-png.txt /mnt/test-dataset/mit-cmu/abc-ground-truth.txt /mnt/classifiers/strongHypothesis-my.txt     /home/ramiro/roc-my.txt
-./test_pavani_classifier /mnt/test-dataset/mit-cmu-png.txt /mnt/test-dataset/mit-cmu/abc-ground-truth.txt /mnt/classifiers/strongHypothesis-pavani.txt /home/ramiro/roc-pavani.txt
-./test_vj_classifier     /mnt/test-dataset/mit-cmu-png.txt /mnt/test-dataset/mit-cmu/abc-ground-truth.txt /mnt/classifiers/strongHypothesis-vj.txt     /home/ramiro/roc-vj.txt
